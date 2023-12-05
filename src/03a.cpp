fad8974b612199f899518702dc2e16bbdd76b9d2
Paul Merrill <pdm@pdm.me>
2023-12-04 23:33:20 -0800

    Day 03a

--- src/03a.cpp
+++ src/03a.cpp
@@ -5,22 +5,78 @@
 #include "util/math2.h"
 #include "util/string2.h"
 #include "util/string-view.h"
+#include "util/vector.h"
+
+bool
+containsSymbol(StringView segment) noexcept {
+    for (char c : segment) {
+        switch (c) {
+            case '#':
+            case '$':
+            case '%':
+            case '&':
+            case '*':
+            case '+':
+            case '-':
+            case '/':
+            case '=':
+            case '@':
+                return true;
+        }
+    }
+    return false;
+}
 
 int
 main() noexcept {
     Flusher f1(sout);
     Flusher f2(serr);
 
-    ReadLines lines("03");
-    if (!lines) {
+    String data;
+    if (!readFile("03", data)) {
         serr << "Could not read input\n";
         return 1;
     }
 
+    // Parse the integers.
+    Vector<StringView> lines;
+    splitStr(lines, data, "\n");
+
     I32 sum = 0;
 
-    for (StringView line = lines.next(); line.data; line = lines.next()) {
-        sum += 1;
+    for (Size lineNum = 0; lineNum < lines.size; lineNum++) {
+        StringView line = lines[lineNum];
+        for (Size i = 0; i < line.size; ) {
+            if ('0' <= line[i] && line[i] <= '9') {
+                I32 x = 0;
+                StringView rest;
+                bool ok = parseI32(&x, &rest, line.substr(i));
+                assert_(ok);
+                Size length = line.size - i - rest.size;
+                Size start = i == 0 ? 0 : i - 1;
+                Size end = i == 0 ? length + 1 : length + 2;
+                end = min(end, line.size - start);
+                bool isAdjacent = false;
+                if (lineNum > 0) {
+                    StringView segment = lines[lineNum - 1].substr(start, end);
+                    isAdjacent = containsSymbol(segment);
+                }
+                if (!isAdjacent) {
+                    StringView segment = line.substr(start, end);
+                    isAdjacent = containsSymbol(segment);
+                }
+                if (!isAdjacent && lineNum + 1 < lines.size) {
+                    StringView segment = lines[lineNum + 1].substr(start, end);
+                    isAdjacent = containsSymbol(segment);
+                }
+                if (isAdjacent) {
+                    sum += x;
+                }
+                i += length;
+            }
+            else
+                i++;
+        }
     }
 
     sout << sum << '\n';
